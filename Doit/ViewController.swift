e4f2d648b59a2257a42bb25dbc07038cb9b5c685
char shu kai <charkai@gmail.com>
2018-02-25 14:58:51 +0800

    Initial Commit

---
+++ Doit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doit
+//
+//  Created by char shu kai on 25/2/2018.
+//  Copyright © 2018年 char shu kai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
